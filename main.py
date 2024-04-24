5cffa92f7c5891bee0449c6561c15f0857fd98c6
Luisa Chiu <luisachiu1@gmail.com>
2024-04-23 23:03:15 -0700

    Created test case functions inside main.py

--- main.py
+++ main.py
@@ -55,18 +55,23 @@ def data_thread():
     while True:
         generate_data()
 
-# if __name__ == "__main__":
-#     plot_thread = threading.Thread(target=plot_thread, daemon = True)
-#     plot_thread.start()
+def test_case1():
+    maze_array1 = np.array(
+    [[0.0, 1.0, 1.0, 0.0],
+    [0.0, 0.0, 0.0, 0.0],
+    [1.0, 1.0, 0.0, 1.0],
+    [0.0, 1.0, 0.0, 0.0]])
+    maze_size = 4
+    marker_filepath = "images/marker8.jpg"
+    maze1 = Maze(maze_array1, marker_filepath, (0,0), (3,3), 180)
+    network = DoubleDQN((120, 120), maze_size)
+    network.train_agent_static(maze1, 200)
+    new_network = DoubleDQN((120, 120), maze_size)
+    new_network.play_game_static(maze1, 100, "model_weights.h5")
 
-#     data_thread = threading.Thread(target=data_thread, daemon = True)
-#     data_thread.start()
-
-#     while True:
-#         time.sleep(1)  # Keep the main thread running
-        
-if __name__ == "__main__":
-    # Using a 4x4 maze:
+# 8 Different mazes
+def test_case3():
+        # Using a 4x4 maze:
     # Testing dynamic mazes:
     maze_array1 = np.array(
     [[0.0, 1.0, 1.0, 0.0],
@@ -150,8 +155,97 @@ if __name__ == "__main__":
     new_network = DoubleDQN((120, 120), maze_size)
     # new_network.play_game_static(maze1, 100, "model_weights.h5")
     new_network.play_game_dynamic([maze1, maze2, maze3, maze4, maze5, maze6, maze7, maze8], 200, "model_weights.h5")
-    # if answer == "n":
-    #     print("Program Exited.")
+
+
+if __name__ == "__main__":
+    test_case1()
+    # # Using a 4x4 maze:
+    # # Testing dynamic mazes:
+    # maze_array1 = np.array(
+    # [[0.0, 1.0, 1.0, 0.0],
+    # [0.0, 0.0, 0.0, 0.0],
+    # [1.0, 1.0, 0.0, 1.0],
+    # [0.0, 1.0, 0.0, 0.0]])
+    # # Different start pt and start orientation than 1
+    # maze_array2 = np.array(
+    # [[0.0, 1.0, 1.0, 0.0],
+    # [0.0, 0.0, 0.0, 0.0],
+    # [1.0, 1.0, 0.0, 1.0],
+    # [0.0, 1.0, 0.0, 0.0]])
+    # # Different end pt than 1
+    # maze_array3 = np.array(
+    # [[0.0, 1.0, 1.0, 0.0],
+    # [0.0, 0.0, 0.0, 0.0],
+    # [1.0, 1.0, 0.0, 1.0],
+    # [0.0, 0.0, 0.0, 0.0]])
+    # maze_array4 = np.array(
+    # [[0.0, 1.0, 1.0, 0.0],
+    # [0.0, 0.0, 0.0, 0.0],
+    # [0.0, 1.0, 1.0, 1.0],
+    # [0.0, 0.0, 0.0, 0.0]])
+    # maze_array5 = np.array(
+    # [[0.0, 1.0, 0.0, 0.0],
+    # [0.0, 0.0, 0.0, 0.0],
+    # [1.0, 1.0, 0.0, 1.0],
+    # [0.0, 0.0, 0.0, 0.0]])
+    # maze_array6 = np.array(
+    # [[0.0, 1.0, 1.0, 0.0],
+    # [0.0, 0.0, 0.0, 0.0],
+    # [1.0, 1.0, 0.0, 1.0],
+    # [0.0, 1.0, 0.0, 0.0]])
+    # maze_array7 = np.array(
+    # [[0.0, 1.0, 1.0, 0.0],
+    # [0.0, 0.0, 0.0, 0.0],
+    # [1.0, 1.0, 0.0, 1.0],
+    # [0.0, 0.0, 0.0, 0.0]])
+    # maze_array8 = np.array(
+    # [[0.0, 1.0, 1.0, 0.0],
+    # [0.0, 0.0, 0.0, 0.0],
+    # [0.0, 1.0, 0.0, 1.0],
+    # [0.0, 0.0, 0.0, 0.0]])
+    # maze_size = 4
+    # # # Using a 8x8 maze:
+    # # maze_array = np.array(
+    # # [[0.0, 1.0, 1.0, 0.0, 1.0, 1.0, 0.0, 0.0],
+    # # [0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0],
+    # # [1.0, 1.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0],
+    # # [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0],
+    # # [0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.0],
+    # # [0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0],
+    # # [0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0],
+    # # [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]])
+    # # maze_size = 8
+
+    # marker_filepath = "images/marker8.jpg"
+    # maze1 = Maze(maze_array1, marker_filepath, (0,0), (3,3), 180)
+    # # maze1.show()
+    # maze2 = Maze(maze_array2, marker_filepath, (1,0), (3,3), 270)
+    # # maze2.show()
+    # maze3 = Maze(maze_array3, marker_filepath, (3,3), (3,0), 180)
+    # # maze3.show()
+    # maze4 = Maze(maze_array4, marker_filepath, (0,0), (3,3), 180)
+    # # maze4.show()
+    # maze5 = Maze(maze_array5, marker_filepath, (0,0), (0,3), 180)
+    # # maze5.show()
+    # maze6 = Maze(maze_array6, marker_filepath, (3,3), (0,0), 90)
+    # # maze6.show()
+    # maze7 = Maze(maze_array7, marker_filepath, (3,3), (1,1), 90)
+    # # maze7.show()
+    # maze8 = Maze(maze_array8, marker_filepath, (3,3), (0,0), 90)
+    # # maze8.show()
+    # network = DoubleDQN((120, 120), maze_size)
+    # # network.train_agent_static(maze1, 200)
+    # network.train_agent_dynamic([maze1, maze2, maze3, maze4, maze5, maze6, maze7, maze8], 3000, heuristics_flag=True)
+
+    # # answer = input("Ready to play the game? y/n: ")
+    # # Create a new object, load weights, and see if it works?
+    # # if answer == "y":
+    # new_network = DoubleDQN((120, 120), maze_size)
+    # # new_network.play_game_static(maze1, 100, "model_weights.h5")
+    # new_network.play_game_dynamic([maze1, maze2, maze3, maze4, maze5, maze6, maze7, maze8], 200, "model_weights.h5")
+    # # if answer == "n":
+    # #     print("Program Exited.")
+
 
 
     # # Testing for saving and loading weights
